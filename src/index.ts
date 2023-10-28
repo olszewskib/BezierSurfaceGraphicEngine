bb22e5fa6e817eb758bb93102e07204060c93aaa
Bartosz Olszewski <bartoszoloolszewski@gmail.com>
2023-10-28 17:27:02 +0200

    code cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,6 @@ import { drawTriangleMesh } from "./triangleMesh";
 const canvas = document.getElementById("mainCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 
-
-//if(ctx){
-//  for(let i=0; i<canvas.height; i++){
-//    for(let j=0; j<canvas.width; j++){
-//      ctx.moveTo(i,j);
-//      ctx.lineTo(i,j+1);
-//    }
-//  }
-//  // so far the fastest way to drwa looking for sth faster
-//  ctx.stroke();
-//}
-
-
 const slider = document.getElementById("precisionSlider") as HTMLInputElement;
 const sliderValue = document.getElementById("sliderValue");
 
