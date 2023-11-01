70406c9b511f67e8ce2b2fd41f7a3523a05afda4
Bartosz Olszewski <bartoszoloolszewski@gmail.com>
2023-11-02 00:32:15 +0100

    refactor

--- src/lib/vertexShader.ts
+++ src/lib/vertexShader.ts
@@ -2,18 +2,15 @@
 export const vertexShaderSourceCode = `#version 300 es
 precision mediump float;
 
+in vec3 vertexColor;
+in vec3 vertexNormal;
+
+
 // ([x,y,z],w) -> w will be set up as defalut 1.0
 in vec4 vertexPosition;
 
 // matrix for transformations
 uniform mat4 matrix;
-
-in vec3 vertexColor;
-in vec3 vertexNormal;
-
-uniform vec2 canvasSize;
-uniform vec2 shapeLocation;
-uniform float shapeSize;
 
 out vec3 fragmentColor;
 out vec3 fragmentNormal;
