2ecd5a28316284ce13c47f959bbba555137eea4c
Bartosz Olszewski <bartoszoloolszewski@gmail.com>
2023-11-05 23:44:49 +0100

    code cleanup

--- src/index.ts
+++ src/index.ts
@@ -279,17 +279,13 @@ function drawTriangles(now: number = 0) {
         gl.bindTexture(gl.TEXTURE_2D,texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]));
 
-        // Asynchronously load an image
         var image = document.getElementById("floor") as HTMLImageElement;
         if(image == null) {
             throw new Error("imageError");
         }
-        //image.addEventListener('load', function() {
-        // Now that the image has loaded make copy it to the texture.
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
         gl.generateMipmap(gl.TEXTURE_2D);
-        //});
         loadTexture = false;
     }
 
